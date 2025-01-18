2f85c179db467d0dbd8c7b2c24c4c0fb3e6a1853
Rishabh503 <rishabhtripathi2022@gmail.com>
2025-01-18 20:41:12 +0530

    added all

---
+++ ecomm/product.models.js
@@ -0,0 +1,41 @@
+import mongoose, { mongo } from 'mongoose'
+
+const productSchema=new mongoose.Schema(
+  {
+    description:{
+      required:true,
+      type:String
+    },
+    name:{
+      required:true,
+      type:String
+    },
+    productImage:{
+      type:String
+    },
+    price:{
+      type:Number,
+      default:0,
+    },
+    stock:{
+      type:Number,
+      default:0,
+    },
+    category:{
+      type:mongoose.Schema.Types.ObjectId,
+      ref:"Category",
+      required:true
+    },
+    owner:{
+      type:mongoose.Schema.Types.ObjectId,
+      ref:"User",
+      required:true
+    }
+
+
+
+
+},{timestamps:true})
+
+
+export const Model=mongoose.model("Model",productSchema)
